86ecb2135ac8e75eb33318ffbe7f42af71daa987
Khai <khaiaw@stanford.edu>
2025-09-08 14:41:03 -0700

    update gcloud script

--- gcloud_rsync.sh
+++ gcloud_rsync.sh
@@ -1,25 +1,9 @@
+# 1. Change to the directory you want to download the files into
+cd /data/datasets/babyview/2025.2/gcloud/
 
-# -i skip existing files, regardless of modification time
-# -n dry run
-
-# -r recursive 
-# -x exclude files
-# -d delete files in destination that are not in source
-# gsutil rsync \
-#     gs://babyview_bing_storage babyview_bing_storage \
-#     -r \
-#     -x ".*\.MP4$" \
-#     -d \
-#     -n
-#     gs://babyview_main_storage babyview_main_storage \
-
-    
-#     -n \
-#     -i \
-
+# 2. Use gcloud storage rsync to sync files
 # gcloud storage command below is the same as the gsutil above
 # gcloud additionally allows a --skip-if-dest-has-newer-mtime flag
-cd /data/datasets/babyview/2025.2/gcloud/
 
 
     # gs://babyview_main_storage babyview_main_storage \
@@ -39,3 +23,24 @@ gcloud storage rsync \
 '''
 
 chmod 777 -R babyview_main_storage/ babyview_bing_storage/ 
+
+
+# ====== Old commands with gsutil ======
+
+# -i skip existing files, regardless of modification time
+# -n dry run
+
+# -r recursive 
+# -x exclude files
+# -d delete files in destination that are not in source
+# gsutil rsync \
+#     gs://babyview_bing_storage babyview_bing_storage \
+#     -r \
+#     -x ".*\.MP4$" \
+#     -d \
+#     -n
+#     gs://babyview_main_storage babyview_main_storage \
+
+    
+#     -n \
+#     -i \
